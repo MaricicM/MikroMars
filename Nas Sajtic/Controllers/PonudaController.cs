9698198ec1fd33e8a52805cadcecabe233ca8da6
MaricicM <30863495+MaricicM@users.noreply.github.com>
2018-04-28 20:18:55 +0200

    28.04 20:18

---
+++ Nas Sajtic/Controllers/PonudaController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Nas_Sajtic.Controllers
+{
+    public class PonudaController : Controller
+    {
+        public ActionResult Ponuda()
+        {
+            return View();
+        }
+
+        public ActionResult SpecPonudaMini()
+        {
+            return View();
+        }
+        public PartialViewResult _Ponuda1()
+        {
+            return PartialView();
+        }
+        public PartialViewResult _Ponuda2()
+        {
+            return PartialView();
+        }
+        public PartialViewResult _Ponuda3()
+        {
+            return PartialView();
+        }
+    }
+}
